a262b3e5a1fe1dc7022fbe3d71ea1cc1818ffb43
Gorkem <gorkemucar2@gmail.com>
2022-12-24 14:56:28 +0300

    Initial commit

---
+++ src/main/java/com/example/loginwithsecurity/LoginWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginwithsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginWithSecurityApplication.class, args);
+    }
+
+}
